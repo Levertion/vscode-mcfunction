be8d6a40f16982c7629a66a6d228ff96d8baade1
Levertion <26185209+Levertion@users.noreply.github.com>
2018-07-22 08:43:36 +0100

    Add prettier and update typescript

--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,62 @@
 "use strict";
 import * as path from "path";
 import { ExtensionContext, window } from "vscode";
-import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from "vscode-languageclient";
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind
+} from "vscode-languageclient";
 
 export function activate(context: ExtensionContext) {
+  // The main file for the server
+  const serverModule = path.join(__dirname, "..", "server", "out", "index.js");
+  // The debug options for the server. Uses a port in about the Minecraft range
+  const debugOptions = { execArgv: ["--nolazy", "--inspect-brk=25575"] };
 
-    // The main file for the server
-    const serverModule = path.join(__dirname, "..", "server", "lib", "index.js");
-    // The debug options for the server. Uses a port in about the Minecraft range
-    const debugOptions = { execArgv: ["--nolazy", "--inspect-brk=25575"] };
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  const serverOptions: ServerOptions = {
+    debug: {
+      module: serverModule,
+      options: debugOptions,
+      transport: TransportKind.ipc
+    },
+    run: { module: serverModule, transport: TransportKind.ipc }
+  };
 
-    // If the extension is launched in debug mode then the debug server options are used
-    // Otherwise the run options are used
-    const serverOptions: ServerOptions = {
-        debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions },
-        run: { module: serverModule, transport: TransportKind.ipc },
-    };
+  // Options to control the language client
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "mcfunction" }],
+    synchronize: {
+      configurationSection: "mcfunction"
+    }
+  };
 
-    // Options to control the language client
-    const clientOptions: LanguageClientOptions = {
-        documentSelector: [{ scheme: "file", language: "mcfunction" }],
-        synchronize: {
-            configurationSection: "mcfunction",
-        },
-    };
+  const client = new LanguageClient(
+    "mcfunction",
+    "Minecraft Function",
+    serverOptions,
+    clientOptions
+  );
 
-    const client = new LanguageClient("mcfunction", "Minecraft Function", serverOptions, clientOptions);
-
-    const disposable = client.start();
-    client.onReady().then(() => {
-        client.onNotification("mcfunction/shutdown", (reason: string) => {
-            window.showWarningMessage(`Minecraft function loader requested to be shutdown with reason ${reason}`);
-            client.stop();
-            disposable.dispose();
-        });
-    }).catch(() => {
-        // Client startup failed. Possibly impossible?
+  const disposable = client.start();
+  client
+    .onReady()
+    .then(() => {
+      client.onNotification("mcfunction/shutdown", (reason: string) => {
+        window.showWarningMessage(
+          `Minecraft function loader requested to be shutdown with reason ${reason}`
+        );
+        client.stop();
+        disposable.dispose();
+      });
+    })
+    .catch(() => {
+      // Client startup failed. Possibly impossible?
     });
 
-    // Push the disposable to the context's subscriptions so that the
-    // client can be deactivated on extension deactivation
-    context.subscriptions.push(disposable);
+  // Push the disposable to the context's subscriptions so that the
+  // client can be deactivated on extension deactivation
+  context.subscriptions.push(disposable);
 }
